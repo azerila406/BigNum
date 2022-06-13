3317383dff78bd4152c5e2f97aa5acf6c555ef5f
Alireza Rahimi <azerila406@gmail.com>
2022-06-13 16:09:37 +0430

    Init

---
+++ BIGNUM.cpp
@@ -0,0 +1,11 @@
+//besmellah
+#include <bits/stdc++.h>
+#define int long long
+#define i64 long long
+using namespace std;
+
+signed main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    return 0;
+}
